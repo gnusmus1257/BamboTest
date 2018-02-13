cbb259c01f9551c453166ee7aa6fa2a3a4082941
Artyom <artemilyin2013@gmail.com>
2018-02-13 16:09:33 +0300

    no message

---
+++ TestBamboo/EntityTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EntityTest.Startup))]
+namespace EntityTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ TestBamboo/TestBamboo/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestBamboo.MvcApplication" Language="C#" %>
